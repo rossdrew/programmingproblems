dcaae0f52b95316e4d0a969ff3793673fecfd921
rd185180 <rd185180@ncr.com>
2015-08-10 17:18:52 +0100

    Problem 7 - Added simple brute force solution answer

--- src/com/rox/problems/Problem7.java
+++ src/com/rox/problems/Problem7.java
@@ -6,6 +6,8 @@ import com.rox.problems.sub.NumberAnalyser;
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
  *
  * What is the 10,001st prime number?
+ *
+ * [Answer = 104743]
  */
 public class Problem7 implements Problem {
     @Override
