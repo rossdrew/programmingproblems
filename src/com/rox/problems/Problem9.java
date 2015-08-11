95dfe46c1fba75961b0d823b3e911a4dc0b86ae3
rd185180 <rd185180@ncr.com>
2015-08-11 16:11:40 +0100

    Problem 9 - Problem stub

---
+++ src/com/rox/problems/Problem9.java
@@ -0,0 +1,23 @@
+package com.rox.problems;
+
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+public class Problem9 implements Problem {
+    @Override
+    public long solution() {
+        return solution(1000L);
+    }
+
+    public long solution(long result)
+    {
+
+        return -1;
+    }
+}
