96934e3995d6560facbcda385165b043cb5219cc
rd185180 <rd185180@ncr.com>
2015-08-13 12:53:47 +0100

    CodingBat.Warmup1 adding some more solutions

--- src/com/rox/prob/codingbat/Warmup1.java
+++ src/com/rox/prob/codingbat/Warmup1.java
@@ -229,4 +229,38 @@ public class Warmup1 {
 
         return first+str+first;
     }
+
+    /**
+     * Given a string, return true if the string starts with "hi" and false otherwise.
+     *
+     * startHi("hi there") → true
+     * startHi("hi") → true
+     * startHi("hello hi") → false
+     */
+     public boolean startHi(String str) {
+        return str.startsWith("hi");
+     }
+
+    /**
+     *
+     * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+     *
+     * icyHot(120, -1) → true
+     * icyHot(-1, 120) → true
+     * icyHot(2, 120) → false
+     */
+    public boolean icyHot(int temp1, int temp2) {
+        return ((temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100));
+    }
+
+    /**
+     * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+     *
+     * in1020(12, 99) → true
+     * in1020(21, 12) → true
+     * in1020(8, 99) → false
+     */
+    public boolean in1020(int a, int b) {
+        return ((a >= 10 && a <=20) || (b >= 10 && b <=20));
+    }
 }
