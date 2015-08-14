a98cdf821f558c6c017296550625c48860cdcffc
rd185180 <rd185180@ncr.com>
2015-08-14 17:09:29 +0100

    CodingBat.Warmup1 Adding another few solutions

--- src/com/rox/prob/codingbat/Warmup1.java
+++ src/com/rox/prob/codingbat/Warmup1.java
@@ -263,4 +263,126 @@ public class Warmup1 {
     public boolean in1020(int a, int b) {
         return ((a >= 10 && a <=20) || (b >= 10 && b <=20));
     }
+
+    /**
+     * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int
+     * values, return true if 1 or more of them are teen.
+     *
+     * hasTeen(13, 20, 10) → true
+     * hasTeen(20, 19, 10) → true
+     * hasTeen(20, 10, 13) → true
+     */
+    public boolean hasTeen(int a, int b, int c) {
+        return ((a >= 13 && a <=19)
+                ||  (b >= 13 && b <=19)
+                ||  (c >= 13 && c <=19));
+    }
+
+    /**
+     * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values,
+     * return true if one or the other is teen, but not both.
+     *
+     * loneTeen(13, 99) → true
+     * loneTeen(21, 19) → true
+     * loneTeen(13, 13) → false
+     */
+     public boolean loneTeen(int a, int b) {
+        boolean aFlag = false;
+        boolean bFlag = false;
+
+        if (a >= 13 && a <= 19)
+        {
+            aFlag = true;
+        }
+
+        if (b >= 13 && b <= 19)
+        {
+            bFlag = true;
+        }
+
+        return ((aFlag || bFlag) && !(aFlag && bFlag));
+     }
+
+    /**
+     * Given a string, if the string "del" appears starting at index 1, return a string where that "del"
+     * has been deleted. Otherwise, return the string unchanged.
+     *
+     * delDel("adelbc") → "abc"
+     * delDel("adelHello") → "aHello"
+     * delDel("adedbc") → "adedbc"
+     */
+     public String delDel(String str) {
+        if (str.length() < 4)
+            return str;
+
+        if (str.substring(1,4).compareTo("del")==0)
+        {
+            return str.substring(0,1) + str.substring(4);
+        }
+        else
+        {
+            return str;
+        }
+     }
+
+    /**
+     * Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+     *
+     * mixStart("mix snacks") → true
+     * mixStart("pix snacks") → true
+     * mixStart("piz snacks") → false
+     */
+     public boolean mixStart(String str) {
+        return str.matches("^.ix.*");
+    }
+
+    /**
+     * Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+     *
+     * startOz("ozymandias") → "oz"
+     * startOz("bzoo") → "z"
+     * startOz("oxx") → "o"
+     */
+     public String startOz(String str) {
+        String r = "";
+
+        if (str.length() > 0 && str.charAt(0)=='o')
+            r+="o";
+
+
+        if (str.length() > 1 && str.charAt(1)=='z')
+            r+="z";
+
+        return r;
+     }
+
+    /**
+     * Given three int values, a b c, return the largest.
+     *
+     * intMax(1, 2, 3) → 3
+     * intMax(1, 3, 2) → 3
+     * intMax(3, 2, 1) → 3
+     */
+     public int intMax(int a, int b, int c) {
+        return Math.max(c,Math.max(a,b));
+     }
+
+    /**
+     * Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
+     *
+     * close10(8, 13) → 8
+     * close10(13, 8) → 8
+     * close10(13, 7) → 0
+     */
+     public int close10(int a, int b) {
+        int an = Math.abs(10-a);
+        int bn = Math.abs(10-b);
+
+        if (an==bn)
+            return 0;
+        else
+        {
+            return an<bn ? a : b;
+        }
+     }
 }
