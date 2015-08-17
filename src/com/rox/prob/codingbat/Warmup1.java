359fcbae85d26fb8014007e5ba27a85200499097
rd185180 <rd185180@ncr.com>
2015-08-17 11:03:38 +0100

    CodingBat.Warmup1 Adding max1020 solution

--- src/com/rox/prob/codingbat/Warmup1.java
+++ src/com/rox/prob/codingbat/Warmup1.java
@@ -397,4 +397,23 @@ public class Warmup1 {
         return ((a >= 30 && a <= 40 && b >= 30 && b <= 40)
              || (a >= 40 && a <= 50 && b >= 40 && b <= 50));
     }
+
+    /**
+     * Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+     *
+     * max1020(11, 19) → 19
+     * max1020(19, 11) → 19
+     * max1020(11, 9) → 11
+     */
+    public int max1020(int a, int b) {
+        int max = (a >= 10 && a <=20) ? a : 0;
+
+        if (b >= 10 && b <=20)
+        {
+            max = Math.max(max, b);
+        }
+
+        return max;
+    }
+
 }
