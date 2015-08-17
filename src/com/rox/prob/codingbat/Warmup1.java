6348a80131f3f910ace14ad37bc24cae151d5dc3
rd185180 <rd185180@ncr.com>
2015-08-17 16:08:59 +0100

    CodingBat.Warmup1 - adding solution to stringE

--- src/com/rox/prob/codingbat/Warmup1.java
+++ src/com/rox/prob/codingbat/Warmup1.java
@@ -419,4 +419,24 @@ public class Warmup1 {
         return max;
     }
 
+    /**
+     * Return true if the given string contains between 1 and 3 'e' chars.
+     *
+     * stringE("Hello") → true
+     * stringE("Heelle") → true
+     * stringE("Heelele") → false
+     */
+    public boolean stringE(String str) {
+        int count = 0;
+        for (char c : str.toCharArray())
+        {
+            if (c == 'e')
+            {
+                count++;
+            }
+        }
+
+        return (count > 0 && count <= 3);
+    }
+
 }
