3acc3abd7aacf76ef18f0f21b85869bb0daf802c
rd185180 <rd185180@ncr.com>
2015-08-18 09:56:01 +0100

    CodingBat.Warmup2 - Adding Warmup2 solution frontTimes

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -26,4 +26,24 @@ public class Warmup2 {
 
         return sb.toString();
     }
+
+    /**
+     * Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+     * or whatever is there if the string is less than length 3. Return n copies of the front;
+     *
+     * frontTimes("Chocolate", 2) → "ChoCho"
+     * frontTimes("Chocolate", 3) → "ChoChoCho"
+     * frontTimes("Abc", 3) → "AbcAbcAbc"
+     */
+    public String frontTimes(String str, int n) {
+        String front = str.substring(0, (str.length() > 3 ? 3 : str.length()));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<n; i++)
+        {
+            sb.append(front);
+        }
+
+        return sb.toString();
+    }
 }
