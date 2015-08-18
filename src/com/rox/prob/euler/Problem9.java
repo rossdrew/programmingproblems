54b05ec7e5a8cb0ea5db4f57e2c7aeadb1421659
rd185180 <rd185180@ncr.com>
2015-08-18 17:02:11 +0100

    ProjectEuler.Problem9 - added solution

--- src/com/rox/prob/euler/Problem9.java
+++ src/com/rox/prob/euler/Problem9.java
@@ -6,10 +6,14 @@ import com.rox.prob.NumericalProblem;
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
  *
  * a^2 + b^2 = c^2
- * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
  *
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
+ *
+ * XXX This could be done much fast eith Euclids Formula but it runs at an acceptable speed as is
+ *
+ * [Answer : 31875000]
  */
 public class Problem9 implements NumericalProblem {
     @Override
@@ -19,7 +23,20 @@ public class Problem9 implements NumericalProblem {
 
     public long solution(long result)
     {
+        for (int a=1; a<result; a++)
+        {
+            for (int b=2; b<result; b++)
+            {
+                for (int c=3; c<result; c++)
+                {
+                    if (a+b+c==result && (a*a) + (b*b) == (c*c))
+                    {
+                        return a*b*c;
+                    }
+                }
+            }
+        }
 
-        return -1;
+        return 0;
     }
 }
