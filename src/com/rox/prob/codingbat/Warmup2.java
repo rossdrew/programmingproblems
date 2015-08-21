3e5be2b031225b34aab939e32ee816bbac163193
rd185180 <rd185180@ncr.com>
2015-08-21 09:39:50 +0100

    CodingBat.Warmup2 - Added solution to countXX

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -46,4 +46,25 @@ public class Warmup2 {
 
         return sb.toString();
     }
+
+    /**
+     * Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+     *
+     * countXX("abcxx") → 1
+     * countXX("xxx") → 2
+     * countXX("xxxx") → 3
+     */
+    int countXX(String str) {
+        int count = 0;
+
+        for (int i=0; i<str.length()-1; i++)
+        {
+            if (str.charAt(i) == 'x' && str.charAt(i+1) == 'x')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
