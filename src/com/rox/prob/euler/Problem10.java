e9a8cc2e36dd94a8b95e1f49b0661dc075687d19
rd185180 <rd185180@ncr.com>
2015-08-25 16:49:32 +0100

    ProjectEuler - Added some comments on optimising Problem 10

--- src/com/rox/prob/euler/Problem10.java
+++ src/com/rox/prob/euler/Problem10.java
@@ -9,6 +9,15 @@ import com.rox.prob.euler.sub.NumberAnalyser;
  * Find the sum of all the primes below two million.
  *
  * [Answer : 142913828922]
+ *
+ * XXX
+ * This could be improved by using The sieve of Eratosthenes
+ *
+ * 1. Make a list of all numbers from 2 to N.
+ * 2. Find the next number p not yet crossed out. This is a prime. If it is greater than the squared root of N, go to 5.
+ * 3. Cross out all multiples of p which are not yet crossed out.
+ * 4. Go to 2.
+ * 5. The numbers not crossed out are the primes not exceeding N.
  */
 public class Problem10 implements NumericalProblem {
     @Override
