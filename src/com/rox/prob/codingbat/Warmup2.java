d70aded17ad3f80f393397c19ae64583ba914d5f
rd185180 <rd185180@ncr.com>
2015-08-27 09:17:54 +0100

    Codingbat.Warmup2 - Adding solution for arrayCount9

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -147,4 +147,24 @@ public class Warmup2 {
         return count;
     }
 
+    /**
+     * Given an array of ints, return the number of 9's in the array.
+     *
+     * arrayCount9({1, 2, 9}) → 1
+     * arrayCount9({1, 9, 9}) → 2
+     * arrayCount9({1, 9, 9, 3, 9}) → 3
+     */
+    public int arrayCount9(int[] nums) {
+        int count=0;
+
+        for (int i : nums)
+        {
+            if (i==9)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
