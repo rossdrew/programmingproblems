548947fd52cd59e21ccd03ed7d87e2ab53280083
Ross Drew <thek1ng13@hotmail.com>
2015-08-29 10:02:53 +0100

    CodingBat.Warmup2 - Added arrayFront9 solution

--- src/com/rox/Main.java
+++ src/com/rox/Main.java
@@ -13,6 +13,6 @@ public class Main {
         /*DEBUG*/System.out.println(p.solution());
 
         Warmup2 w = new Warmup2();
-        /*DEBUG*/System.out.println(w.last2("hixxhi"));
+        /*DEBUG*/System.out.println(w.arrayFront9(new int[] {1,9}));
     }
 }
--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -167,4 +167,26 @@ public class Warmup2 {
 
         return count;
     }
+
+    /**
+     * Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+     * The array length may be less than 4.
+     *
+     * arrayFront9({1, 2, 9, 3, 4}) → true
+     * arrayFront9({1, 2, 3, 4, 9}) → false
+     * arrayFront9({1, 2, 3, 4, 5}) → false
+     */
+    public boolean arrayFront9(int[] nums) {
+        int length = (nums.length < 4) ? nums.length : 4;
+
+        for (int i=0; i<length; i++)
+        {
+            if (nums[i] == 9) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
