4c73ae0aa4e45db76c778b94494e9376995ca5e2
Ross Drew <thek1ng13@hotmail.com>
2015-08-30 20:44:33 +0100

    CodingBat.Warmup2 - Added array123 solution

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -189,4 +189,30 @@ public class Warmup2 {
         return false;
     }
 
+    /**
+     * Given an array of ints, return true if .. 1, 2, 3, .. appears in the array somewhere.
+     *
+     * array123({1, 1, 2, 3, 1}) → true
+     * array123({1, 1, 2, 4, 1}) → false
+     * array123({1, 1, 2, 1, 2, 3}) → true
+     */
+    public boolean array123(int[] nums) {
+        for (int i=0; i<nums.length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                if (i+2 > nums.length)
+                {
+                    return false;
+                }
+                else if (nums[i+1] == 2 && nums[i+2] == 3)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
