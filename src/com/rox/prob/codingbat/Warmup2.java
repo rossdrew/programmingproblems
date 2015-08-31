106d8f56859da5731c234287559e7d23ceb529f9
rd185180 <rd185180@ncr.com>
2015-08-31 10:29:49 +0100

    CodingBat.Warmup2 - Added solution to stringMatch

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -217,4 +217,26 @@ public class Warmup2 {
         return false;
     }
 
+    /**
+     * Given 2 strings, a and b, return the number of the positions where they contain the same
+     * length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+     * substrings appear in the same place in both strings.
+     *
+     * stringMatch("xxcaazz", "xxbaaz") → 3
+     * stringMatch("abc", "abc") → 2
+     * stringMatch("abc", "axc") → 0
+     */
+    public int stringMatch(String a, String b) {
+        int count=0;
+
+        for (int i=0; (i<a.length()-1 && i<b.length()-1); i++)
+        {
+            if (a.charAt(i) == b.charAt(i) && a.charAt(i+1) == b.charAt(i+1))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
