4fc9b167d568a3c08218e9c0b1c4edcaf8ae8ebe
Ross Drew <thek1ng13@hotmail.com>
2015-09-05 08:49:46 +0100

    CodingBat.Recursion1 - Added solution to triangle

--- src/com/rox/prob/codingbat/Recursion1.java
+++ src/com/rox/prob/codingbat/Recursion1.java
@@ -35,4 +35,23 @@ public class Recursion1 {
 
         return 2 + bunnyEars(bunnies-1);
     }
+
+    /**
+     * We have triangle made of blocks. The topmost row has 1 block, the next row down has 2
+     * blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or
+     * multiplication) the total number of blocks in such a triangle with the given number
+     * of rows.
+     *
+     * triangle(0) → 0
+     * triangle(1) → 1
+     * triangle(2) → 3
+     */
+    public int triangle(int rows) {
+        if (rows == 0)
+        {
+            return 0;
+        }
+
+        return rows+triangle(rows-1);
+    }
 }
