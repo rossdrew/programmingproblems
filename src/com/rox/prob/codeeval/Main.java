38a91ede3260dc7c8dda2b3aa631308029723b2b
Ross Drew <thek1ng13@hotmail.com>
2015-09-12 08:07:11 +0100

    CodeEval - Adding template class for building solutions to submit

---
+++ src/com/rox/prob/codeeval/Main.java
@@ -0,0 +1,15 @@
+package com.rox.prob.codeeval;
+
+import java.io.*;
+
+/**
+ * CodeEval Template
+ */
+public class Main {
+    Main(){
+
+    }
+
+    public static void main (String[] args) throws IOException {
+    }
+}
