0de7311d558eeb6e77705f1dda8a7777ca96a189
Ross Drew <thek1ng13@hotmail.com>
2015-09-14 22:28:34 +0100

    CodingBat.Recursion1 - Adding solution to changePi

--- src/com/rox/prob/codingbat/Recursion1.java
+++ src/com/rox/prob/codingbat/Recursion1.java
@@ -235,4 +235,27 @@ public class Recursion1 {
 
         return thisChar + changeXY(str.substring(1));
     }
+
+    /**
+     * Given a string, compute recursively (no loops) a new string
+     * where all appearances of "pi" have been replaced by "3.14".
+     *
+     * changePi("xpix") → "x3.14x"
+     * changePi("pipi") → "3.143.14"
+     * changePi("pip") → "3.14p"
+     */
+    public String changePi(String str) {
+        String searchString = "pi";
+        String replaceString = "3.14";
+
+        if (str.length() < 3){
+            return (str.compareTo(searchString)==0 ? replaceString : str);
+        }
+
+        if (str.substring(0, 2).compareTo(searchString) == 0){
+            return replaceString + changePi(str.substring(2));
+        }else{
+            return str.substring(0,1) + changePi(str.substring(1));
+        }
+    }
 }
