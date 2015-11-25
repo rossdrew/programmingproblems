0146943ad39797e3794cf9ed629829ca6a7f8d83
rossdrew <thek1ng13@hotmail.com>
2015-11-25 14:33:44 +0000

    Warmup2 - Solution to stringX

--- src/com/rox/prob/codingbat/Warmup2.java
+++ src/com/rox/prob/codingbat/Warmup2.java
@@ -239,4 +239,19 @@ public class Warmup2 {
 
         return count;
     }
+
+    /**
+     * Given a string, return a version where all the "x" have been removed. 
+     * Except an "x" at the very start or end should not be removed.
+     *
+     * stringX("xxHxix") → "xHix"
+     * stringX("abxxxcd") → "abcd"
+     * stringX("xabxxxcdx") → "xabcdx"
+     */
+    public String stringX(String str) {
+        if (str.length() < 3)
+            return str;
+        String mid = str.substring(1, str.length()-1).replaceAll("x","");
+        return str.charAt(0) + mid + str.charAt(str.length()-1);
+    }
 }
