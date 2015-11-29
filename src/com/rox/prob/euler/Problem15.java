d5013693baaf6966bf979cf2619597a0a58192db
rossdrew <thek1ng13@hotmail.com>
2015-11-29 19:20:08 +0000

    Euler - Project15 - Possible solution which seems to return a wrong answer

--- src/com/rox/prob/euler/Problem15.java
+++ src/com/rox/prob/euler/Problem15.java
@@ -1,6 +1,8 @@
 package com.rox.prob.euler;
 
 import com.rox.prob.NumericalProblem;
+import com.rox.prob.common.struct.Graph;
+import com.rox.prob.common.struct.Vertice;
 
 /**
  * Starting in the top left corner of a 2×2 grid, and only being able
@@ -9,15 +11,27 @@ import com.rox.prob.NumericalProblem;
  *
  * How many such routes are there through a 20×20 grid?
  *
+ * [Answer : 407575348]
  */
 public class Problem15 implements NumericalProblem<Integer>{
     @Override
     public Integer solution() {
-        return solution(20);
+        return solution(10);
     }
 
     public Integer solution(int squareSize){
-        
-        return -1;
+        int[][] square = new int[squareSize+1][squareSize+1];
+
+        for (int x=0; x <= squareSize; x++){
+            for (int y=0; y <= squareSize; y++){
+                if (x == 0 || y == 0){
+                    square[x][y] = 1;
+                }else{
+                    square[x][y] = square[x][y-1] + square[x-1][y];
+                }
+            }
+        }
+
+        return square[squareSize][squareSize];
     }
 }
