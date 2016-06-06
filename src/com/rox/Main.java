51f13d187d71da36c3cda947dc046c3475c8e1b7
rossdrew <thek1ng13@hotmail.com>
2016-06-06 14:24:17 +0100

    CodingBat.Map1: mapBully and mapShare solutions

--- src/com/rox/Main.java
+++ src/com/rox/Main.java
@@ -1,22 +1,35 @@
 package com.rox;
 
 
+import com.rox.prob.codingbat.Map1;
 import com.rox.prob.codingbat.Recursion1;
 import com.rox.prob.codingbat.Warmup2;
 import com.rox.prob.common.NumberAnalyser;
 import com.rox.prob.euler.*;
 import com.rox.prob.NumericalProblem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        NumericalProblem p = new Problem19();
-        /*DEBUG*/System.out.println(p.solution());
+        //NumericalProblem p = new Problem19();
+        /*DEBUG*///System.out.println(p.solution());
 
         //Warmup2 w = new Warmup2();
-        /*DEBUG*///System.out.println(w.stringX("xxHxix"));
+        //DEBUG*///System.out.println(w.stringX("xxHxix"));
 
         //Recursion1 r = new Recursion1();
         /*DEBUG*///System.out.println(r.allStar("Thisisatest"));
+
+        Map1 m = new Map1();
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("b","bbb");  //{"b": "bbb", "c": "ccc", "a": "aaa"}
+        testMap.put("c","ccc");
+        testMap.put("a","aaa");
+        m.mapShare(testMap);
+        /*DEBUG*/
+        System.out.println(testMap.toString());
     }
 }
---
+++ src/com/rox/prob/codingbat/Map1.java
@@ -0,0 +1,48 @@
+package com.rox.prob.codingbat;
+
+import java.util.Map;
+
+/**
+ * Basic Map get()/put(), no loops. See the Java Map Introduction for help.
+ */
+public class Map1 {
+    /**
+     * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to
+     * have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking
+     * the value of "a".
+     *
+     * mapBully({"b": "dirt", "a": "candy"}) → {"b": "candy", "a": ""}
+     * mapBully({"a": "candy"}) → {"b": "candy", "a": ""}
+     * mapBully({"b": "carrot", "c": "meh", "a": "candy"}) → {"b": "candy", "c": "meh", "a": ""}
+     */
+    public Map<String, String> mapBully(Map<String, String> map) {
+        String a = map.get("a");
+
+        if (a!=null && !a.isEmpty()){
+            map.put("b", a);
+            map.put("a", "");
+        }
+
+        return map;
+    }
+
+    /**
+     * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to
+     * have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+     *
+     * mapShare({"b": "bbb", "c": "ccc", "a": "aaa"}) → {"b": "aaa", "a": "aaa"}
+     * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+     * mapShare({"d": "hi", "c": "meh", "a": "aaa"}) → {"d": "hi", "b": "aaa", "a": "aaa"}
+     */
+    public Map<String, String> mapShare(Map<String, String> map) {
+        String a = map.get("a");
+
+        if (a!=null && !a.isEmpty()){
+            map.put("b", a);
+        }
+
+        map.remove("c");
+        return map;
+    }
+
+}
