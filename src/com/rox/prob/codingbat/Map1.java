05ee437b9e909aaf66768eb2f158de8bf8428007
rossdrew <thek1ng13@hotmail.com>
2016-06-10 09:04:31 +0100

    CodingBat.Map1: Problem definition for topping2

--- src/com/rox/prob/codingbat/Map1.java
+++ src/com/rox/prob/codingbat/Map1.java
@@ -84,5 +84,17 @@ public class Map1 {
         return map;
     }
 
+    /**
+     * Given a map of food keys and their topping values, modify and return the map as follows: if the key
+     * "ice cream" has a value, set that as the value for the key "yogurt" also. If the key "spinach" has
+     * a value, change that value to "nuts".
+     *
+     * topping2({"ice cream": "cherry"}) → {"yogurt": "cherry", "ice cream": "cherry"}
+     * topping2({"spinach": "dirt", "ice cream": "cherry"}) → {"yogurt": "cherry", "spinach": "nuts", "ice cream": "cherry"}
+     * topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
+     */
+    public Map<String, String> topping2(Map<String, String> map) {
+        return null;
+    }
 
 }
