a1a5db57774962a816598fbe248e2d2f57faeb54
rossdrew <ross.drew@symphonicsoft.com>
2017-12-04 10:20:48 +0000

    [AoC] Day 3, part 1, 2017 problem solution
    This is some ugly Java code but I found this problem tough and
    concentrated on the problem, rather than the design.  I can re-learn and
    refine the process during a refactor later...hopefully

--- src/main/java/com/rox/prob/adventofcode/y2017/Day3.java
+++ src/main/java/com/rox/prob/adventofcode/y2017/Day3.java
@@ -30,13 +30,70 @@ package com.rox.prob.adventofcode.y2017;
  *
  */
 public class Day3 {
-    public long solution(long cellNumber){
-        //First (n) odd square (o) above our number gives the bottom right corner
-        //n:1,2,3,4,5,6 ,7 ,8 ,9, ...
-        //o:1,3,5,7,9,11,13,15,17,...
-        //armLen: (n*2) +1
+    /**
+     * - Find the nth odd square above our number (the spiral ceiling)
+     * - Step back along the spiral to the given cellNumber, creating a square
+     * - Find the manhattan distance from the centre of the created square (cell 1) to the cellNumber
+     */
+    public long solution(int cellNumber){
+        if (cellNumber == 1)
+            return 0;
 
-        return -1;
+        /** Which ring of the spiral we are on (1,3,5,7...), also the size of the arm */
+        int spiralValue = 1;
+        /** The ceiling of the ring (highest value) */
+        int spiralCeiling = 1;
+
+        while (spiralCeiling < cellNumber){
+            while (++spiralValue % 2 == 0){}//Next odd number
+            spiralCeiling = spiralValue * spiralValue;
+        }
+
+        int stepsAroundToCell = spiralCeiling - cellNumber;
+        int stepsAlongArm = stepsAroundToCell % (spiralValue-1);
+        int armNumber = stepsAroundToCell / (spiralValue-1);
+
+        //System.out.println("f(" + cellNumber + ") -> " + spiralValue + "x" + spiralValue + "=" + spiralCeiling + "\t armLength="+spiralValue + "\t Directions (" + stepsAroundToCell + " steps) = arm: " + armNumber + ", stepsAlongArm = " + stepsAlongArm);
+
+        //get the offset from cell spiralIndex X spiralIndex
+        int x = spiralValue-1;
+        int y = spiralValue-1;
+        switch (armNumber){
+            case 0: //bottom arm
+                System.out.println("Bottom arm...");
+                x -= stepsAlongArm;
+                break;
+
+            case 1: //left arm
+                System.out.println("Left arm...");
+                x -= (spiralValue-1);
+                y -= (stepsAlongArm);
+                break;
+
+            case 2: //top arm
+                System.out.println("Top arm...");
+                x += (stepsAlongArm-(spiralValue-1));
+                y -= ((spiralValue-1));
+                break;
+
+            case 3: //right arm
+                System.out.println("Right arm...");
+                y += (stepsAlongArm-(spiralValue-1));
+                break;
+        }
+        //System.out.println("Offset Calculated as (" + y + ", " + x + ") for " + cellNumber);
+
+        int sourceX = spiralValue / 2;
+        int sourceY = spiralValue / 2;
+
+        //manhattan distance between offset from ceiling and source
+        int xDistance = (sourceX > x) ? (sourceX - x) : (x - sourceX);
+        int yDistance = (sourceY > y) ? (sourceY - y) : (y - sourceY);
+        int manhattanDistance = xDistance+yDistance;
+
+        //System.out.println("Distance from (" + sourceY + "," + sourceX + ") is (" + yDistance + ", " + xDistance + ") -> " + manhattanDistance);
+
+        return manhattanDistance;
     }
 
 }
--- src/test/java/com/rox/prob/adventofcode/y2017/Day3Test.java
+++ src/test/java/com/rox/prob/adventofcode/y2017/Day3Test.java
@@ -1,13 +1,16 @@
 package com.rox.prob.adventofcode.y2017;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Day3Test {
     @Test
-    @Ignore("Work in progress")
+    public void testPart1Solution(){
+        Day3 day = new Day3();
+        assertEquals(552, day.solution(325489));
+    }
+    @Test
     public void testPart1Examples(){
         Day3 day = new Day3();
         assertEquals(0, day.solution(1));
@@ -17,9 +20,36 @@ public class Day3Test {
     }
 
     @Test
-    @Ignore("Work in progress")
-    public void testBlock(){
+    public void testPart1OffCorner(){
         Day3 day = new Day3();
-        assertEquals(31, day.solution(50));
+        assertEquals(3, day.solution(24));
+        assertEquals(3, day.solution(20));
+        assertEquals(3, day.solution(16));
+        assertEquals(3, day.solution(12));
+    }
+
+    @Test
+    public void testPart1Corners(){
+        Day3 day = new Day3();
+        assertEquals(4, day.solution(25));
+        assertEquals(4, day.solution(21));
+        assertEquals(4, day.solution(17));
+        assertEquals(4, day.solution(13));
+    }
+
+    @Test
+    public void testPart1Edges(){
+        Day3 day = new Day3();
+        assertEquals(2, day.solution(23));
+        assertEquals(2, day.solution(19));
+        assertEquals(2, day.solution(15));
+        assertEquals(2, day.solution(11));
+    }
+
+    @Test
+    public void testPart1InnerRing(){
+        Day3 day = new Day3();
+        assertEquals(2, day.solution(5));
+        assertEquals(1, day.solution(2));
     }
 }
