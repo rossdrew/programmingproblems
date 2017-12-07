9ac6476b13c84d620b2eca32e3b6bfdc9b9cd92e
rossdrew <ross.drew@symphonicsoft.com>
2017-12-07 09:38:50 +0000

    [AoC] Day 7, Part 1, problem description and example test case

---
+++ src/main/java/com/rox/prob/adventofcode/y2017/Day7.java
@@ -0,0 +1,8 @@
+package com.rox.prob.adventofcode.y2017;
+
+public class Day7 {
+    public String part1Solution(String input) {
+
+        return null; //TODO
+    }
+}
---
+++ src/test/java/com/rox/prob/adventofcode/y2017/Day7Test.java
@@ -0,0 +1,26 @@
+package com.rox.prob.adventofcode.y2017;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day7Test {
+    private static final String INPUT = "        pbga (66)\n"+
+                                        "        xhth (57)\n"+
+                                        "        ebii (61)\n"+
+                                        "        havc (66)\n"+
+                                        "        ktlj (57)\n"+
+                                        "        fwft (72) -> ktlj, cntj, xhth\n"+
+                                        "        qoyq (66)\n"+
+                                        "        padx (45) -> pbga, havc, qoyq\n"+
+                                        "        tknk (41) -> ugml, padx, fwft\n"+
+                                        "        jptl (61)\n"+
+                                        "        ugml (68) -> gyxo, ebii, jptl\n"+
+                                        "        gyxo (61)\n"+
+                                        "        cntj (57)";
+    @Test
+    public void testPart1Solution(){
+        Day7 day = new Day7();
+        assertEquals("tknk", day.part1Solution(INPUT));
+    }
+}
