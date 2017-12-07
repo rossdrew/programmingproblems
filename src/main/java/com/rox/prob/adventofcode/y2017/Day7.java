a06cf11024152dbda20738f5d85ec27fc9d4d176
rossdrew <ross.drew@symphonicsoft.com>
2017-12-07 09:42:34 +0000

    [AoC] Day 7, Part 1, problem input and test case

--- src/main/java/com/rox/prob/adventofcode/y2017/Day7.java
+++ src/main/java/com/rox/prob/adventofcode/y2017/Day7.java
@@ -2,6 +2,8 @@ package com.rox.prob.adventofcode.y2017;
 
 public class Day7 {
     public String part1Solution(String input) {
+        final String[] call = input.split("\\s");
+
 
         return null; //TODO
     }
