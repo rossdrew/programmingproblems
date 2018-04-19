a97bb84033b475f22e8b8b19ca352097bd308090
rossdrew <thek1ng13@hotmail.com>
2018-04-19 15:29:36 +0100

    Euler: Changing the name of problem 22 file name

--- src/main/java/com/rox/prob/euler/Problem22.java
+++ src/main/java/com/rox/prob/euler/Problem22.java
@@ -3,7 +3,7 @@ package com.rox.prob.euler;
 import com.rox.prob.NumericalProblem;
 
 /**
- * Using names.txt ('p022_names.txt'), a 46K text file containing over
+ * Using names.txt ('euler_p022_names.txt'), a 46K text file containing over
  * five-thousand first names, begin by sorting it into alphabetical order. Then working out the
  * alphabetical value for each name, multiply this value by its alphabetical position in the
  * list to obtain a name score.
