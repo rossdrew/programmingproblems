e762b0d815f90e81e00e0330b8654b9d6dc86f59
rossdrew <thek1ng13@hotmail.com>
2018-04-20 09:50:49 +0100

    Euler.Problem22: Problem solution

--- src/main/java/com/rox/prob/euler/Problem22.java
+++ src/main/java/com/rox/prob/euler/Problem22.java
@@ -2,6 +2,11 @@ package com.rox.prob.euler;
 
 import com.rox.prob.NumericalProblem;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.*;
+
 /**
  * Using names.txt ('euler_p022_names.txt'), a 46K text file containing over
  * five-thousand first names, begin by sorting it into alphabetical order. Then working out the
@@ -13,9 +18,48 @@ import com.rox.prob.NumericalProblem;
  *
  * What is the total of all the name scores in the file?
  */
-public class Problem22 implements NumericalProblem<Long> {
+public class Problem22 implements NumericalProblem<BigDecimal> {
+    private Scanner fileScanner;
+
     @Override
-    public Long solution() {
-        return null;
+    public BigDecimal solution() {
+        return solution("src/main/resources/euler_p022_names.txt");
+    }
+
+    public BigDecimal solution(final String filename){
+        BigDecimal result = BigDecimal.ZERO;
+
+        final String[] names = getNames(filename);
+
+        final Map<String, BigDecimal> sortedNames = new TreeMap<>();
+        for (String name : names) {
+            sortedNames.put(name.replaceAll("\"", "").trim(), BigDecimal.ZERO);
+        }
+
+        int nameIndex = 1;
+        for (final String sortedName : sortedNames.keySet()) {
+            char[] nameChars = sortedName.toCharArray();
+            int charSum = 0;
+            for (char nameChar : nameChars)
+                charSum += Character.getNumericValue(nameChar) - 9; // 'A' == 10
+
+            final BigDecimal value = BigDecimal.valueOf(((long)nameIndex++) * charSum);
+            sortedNames.put(sortedName, value);
+            result = result.add(value);
+        }
+
+        return result;
+    }
+
+    private String[] getNames(String filename) {
+        try {
+            File file = new File(filename);
+            fileScanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("Invalid file input.");
+        }
+
+        final String input = fileScanner.next();
+        return input.split(",");
     }
 }
