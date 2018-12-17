5a5d10bcdb4f39332673053fc895266c82dce98e
rossdrew <thek1ng13@hotmail.com>
2018-12-17 16:34:15 +0000

    [AoC 2018] Day4, Part 2. Solution.
    This was really simple after the setup done in Part 1.  It was just
    running another maxBy call over the sleep summaries.  I still don't like
    the use of all the `!!` but hey-ho

--- src/main/kotlin/com/rox/adventofcode/y2018/Day4.kt
+++ src/main/kotlin/com/rox/adventofcode/y2018/Day4.kt
@@ -1141,7 +1141,7 @@ fun main(args: Array<String>) {
     val sleepLogEntries = inputA.split('\n')
 
     println("Part A: ${solutionA(sleepLogEntries)}")
-    println("Part B: ${solutionB()}")
+    println("Part B: ${solutionB(sleepLogEntries)}")
 }
 
 /**
@@ -1216,10 +1216,17 @@ fun main(args: Array<String>) {
  * Answer: 131469
  */
 fun solutionA(logEntries: List<String>): Any {
+    val sleepSummaries = setup(logEntries)
+    val sleepiestGuard = findSleepiestGuard(sleepSummaries)
+    val mostPopularMinute: Int = sleepSummaries[sleepiestGuard]?.mostPopularMinute()!!
+    return sleepiestGuard * mostPopularMinute
+}
+
+private fun setup(logEntries: List<String>): MutableMap<Int, SleepSummary> {
     var currentGuardId = -1
 
     val sortedEntries = logEntries.sorted().map { logEntry ->
-        with(LogEntry.parse(logEntry, currentGuardId)){
+        with(LogEntry.parse(logEntry, currentGuardId)) {
             currentGuardId = this.guardId
             return@map time to this
         }
@@ -1228,27 +1235,39 @@ fun solutionA(logEntries: List<String>): Any {
     val sleepSummaries = mutableMapOf<Int, SleepSummary>()
     val entryIterator = sortedEntries.iterator()
 
-    while (entryIterator.hasNext()){
+    while (entryIterator.hasNext()) {
         val nextPart1 = entryIterator.next().value
         if (nextPart1.event == LogEntry.Event.SLEEP) {
             val nextPart2 = entryIterator.next().value
-            if (nextPart2.event == LogEntry.Event.WAKE){
+            if (nextPart2.event == LogEntry.Event.WAKE) {
                 sleepSummaries.update(nextPart1.guardId, nextPart1.time, nextPart2.time)
             }
         }
     }
-
-    val sleepiestGuard = findSleepiestGuard(sleepSummaries)
-    val mostPopularMinute: Int = sleepSummaries[sleepiestGuard]?.mostPopularMinute()!!
-    return sleepiestGuard * mostPopularMinute
+    return sleepSummaries
 }
 
 fun findSleepiestGuard(sleepSummaries: Map<Int, SleepSummary>): Int {
     return sleepSummaries.maxBy { it.value.sleepTally() }!!.key
 }
 
-fun solutionB(): Any {
-    return "Not implemented"
+/**
+ * --- Part Two ---
+ * Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+ *
+ * In the example above, Guard #99 spent minute 45 asleep more than any other guard or minute - three times in total.
+ * (In all other cases, any guard spent any minute asleep at most twice.)
+ *
+ * What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would
+ * be 99 * 45 = 4455.)
+ *
+ * Answer: 96951
+ */
+fun solutionB(logEntries: List<String>): Any {
+    val sleepSummaries = setup(logEntries)
+
+    val maxBy = sleepSummaries.keys.maxBy { sleepSummaries[it]!!.mostPopularMinute() } as Int
+    return maxBy * sleepSummaries[maxBy]!!.mostPopularMinute()
 }
 
 /**
