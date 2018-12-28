7a7e19e39d4695edd49db18babd2718a2b008607
rossdrew <thek1ng13@hotmail.com>
2018-12-28 03:20:01 +0000

    [AoC 2018] Moving common code out from Day 3, to be used in Day 6

---
+++ src/main/kotlin/com/rox/adventofcode/y2018/Library.kt
@@ -0,0 +1,24 @@
+package com.rox.adventofcode.y2018
+
+/**
+ * Represents a 2D cartesian coordinate
+ */
+class Coordinate(val x: Int, val y: Int) {
+    override fun toString(): String {
+        return "${x}x$y"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Coordinate)
+            return false
+
+        if (other.x == x && other.y == y )
+            return true
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * x + y
+    }
+}
