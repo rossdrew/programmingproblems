e0b6e5883c1b8731475250bd11590e013a44a1f0
rossdrew <thek1ng13@hotmail.com>
2019-12-07 10:06:08 +0000

    [Aoc 2019] Problem 4: Part A solution and Part B problem and solutio

--- src/main/kotlin/com/rox/adventofcode/y2019/Day4.kt
+++ src/main/kotlin/com/rox/adventofcode/y2019/Day4.kt
@@ -25,27 +25,67 @@ Other than the range rule, the following are true:
 123789 does not meet these criteria (no double).
 How many different passwords within the range given in your puzzle input meet these criteria?
 
- Answer: WRONG-TOO-HIGH 917
+ Answer: 910
  */
 private fun solutionA(input : String) : Int? {
     val range = input.split('-').map { it.toInt() }
 
     val workingRange = range[0]..range[1]
-    val filterdRange = workingRange.filter { candidate ->
-        candidate.toString().length == 6 &&
-        candidate.toString().toCharArray().distinct().isNotEmpty() //&&
-        candidate.toString().toCharArray().sortedBy{it} == candidate.toString().toList()
+    val filteredRange = workingRange.map { it.toString() }.filter { candidate ->
+        candidate.length == 6 &&
+        hasRepeatingDigit(candidate) &&
+        candidate.toCharArray().sortedBy{it} == candidate.toList()
     }
 
-    println(workingRange)
-    println("${filterdRange.size} items: $filterdRange")
+    return filteredRange.size
+}
 
-    return filterdRange.size
+private fun hasRepeatingDigit(candidate: String): Boolean{
+    var lastCharacter = candidate[0]
+    for (i in 1 until candidate.length){
+        if (candidate[i] == lastCharacter) {
+            return true
+        } else {
+            lastCharacter = candidate[i]
+        }
+    }
+    return false
 }
 
 /**
+ * --- Part Two ---
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of
+matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+
+ ANSWER: 598
  */
 private fun solutionB(input : String) : Int? {
-    val range = input.split('-').map { it.toDouble() }
-    throw java.lang.RuntimeException("Not Implemented")
+    val range = input.split('-').map { it.toInt() }
+
+    val workingRange = range[0]..range[1]
+    val filteredRange = workingRange.map { it.toString() }.filter { candidate ->
+        candidate.length == 6 &&
+        hasSinglyRepeatedDigit(candidate) &&
+        candidate.toCharArray().sortedBy{it} == candidate.toList()
+    }
+
+    return filteredRange.size
+}
+
+/**
+ * Thought of another, nicer way to do the <code>hasRepeatingDigit</code> work
+ */
+private fun hasSinglyRepeatedDigit(candidate: String): Boolean{
+    candidate.toCharArray().distinct().forEach{char ->
+        if (candidate.split(char).size - 1 == 2)
+            return true
+    }
+    return false
 }
