4819d3f5bb94b9ed794271be91b46cb544cbb13f
rossdrew <thek1ng13@hotmail.com>
2021-05-12 16:34:08 +0100

    CodingBat.Recursion1.count11: Solution

--- src/main/java/com/rox/prob/codingbat/Recursion1.java
+++ src/main/java/com/rox/prob/codingbat/Recursion1.java
@@ -419,4 +419,23 @@ public class Recursion1 {
 
         return ((str.startsWith("abc") || str.startsWith("aba")) ? 1 : 0) + countAbc(str.substring(1));
     }
+
+    /**
+     * Given a string, compute recursively (no loops) the number of "11" substrings in
+     * the string. The "11" substrings should not overlap.
+     *
+     *
+     * count11("11abc11") → 2
+     * count11("abc11x11x11") → 3
+     * count11("111") → 1
+     */
+    public int count11(String str){
+        if (str.length() == 0)
+            return 0;
+
+        if (str.startsWith("11"))
+            return 1 + count11(str.substring(2));
+        else
+            return count11(str.substring(1));
+    }
 }
