c1fa2e0e77a49b76e7843c10e2ba8006b840ae62
rossdrew <thek1ng13@hotmail.com>
2021-05-13 09:09:18 +0100

    CodingBat.Recursion1.stringClean: Sketchy solution
    This works but nesting a recursive call in a recursive call isn't very
    elegant, there's likely a nicer way.

--- src/main/java/com/rox/prob/codingbat/Recursion1.java
+++ src/main/java/com/rox/prob/codingbat/Recursion1.java
@@ -438,4 +438,23 @@ public class Recursion1 {
         else
             return count11(str.substring(1));
     }
+
+    /**
+     * Given a string, return recursively a "cleaned" string where adjacent chars that are the
+     * same have been reduced to a single char. So "yyzzza" yields "yza".
+     *
+     * stringClean("yyzzza") → "yza"
+     * stringClean("abbbcdd") → "abcd"
+     * stringClean("Hello") → "Helo"
+     */
+    public String stringClean(String str) {
+        if (str.length()<2)
+            return str;
+
+        if (str.substring(0,1).equals(str.substring(1,2))){
+            return stringClean(str.substring(0,1) + stringClean(str.substring(2)));
+        }else{
+            return str.substring(0,1) + stringClean(str.substring(1));
+        }
+    }
 }
