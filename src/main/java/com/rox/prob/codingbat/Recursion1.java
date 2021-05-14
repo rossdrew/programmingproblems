e57af5f93ce79a6a08770d990416e6645a3bf37b
rossdrew <thek1ng13@hotmail.com>
2021-05-14 14:41:52 +0100

    CodingBat.Recursion1.countHi2: Solution

--- src/main/java/com/rox/prob/codingbat/Recursion1.java
+++ src/main/java/com/rox/prob/codingbat/Recursion1.java
@@ -457,4 +457,29 @@ public class Recursion1 {
             return str.substring(0,1) + stringClean(str.substring(1));
         }
     }
+
+    /**
+     * Given a string, compute recursively the number of times lowercase "hi" appears in the
+     * string, however do not count "hi" that have an 'x' immedately before them.
+     *
+     * countHi2("ahixhi") → 1
+     * countHi2("ahibhi") → 2
+     * countHi2("xhixhi") → 0
+     */
+    public int countHi2(String str) {
+        if (str.length() == 0)
+            return 0;
+
+        if (str.length() > 2 && str.substring(0, 3).equals("xhi")){
+            return countHi2(str.substring(3));
+        }else if (str.length() > 1 && str.substring(0,2).equals("hi")){
+            return 1 + countHi2(str.substring(2));
+        }else{
+            return countHi2(str.substring(1));
+        }
+    }
+
+    public static void main(String[] args){
+        new Recursion1().countHi2("ahibhi");
+    }
 }
