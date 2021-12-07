f2b2ece23284aeab3a1cd7626f80dd67722aed9a
rossdrew <thek1ng13@hotmail.com>
2021-12-07 10:11:41 +0000

    [AoC 2021 Day 7] Forgot the answer

--- src/main/kotlin/com/rox/adventofcode/y2021/Day7.kt
+++ src/main/kotlin/com/rox/adventofcode/y2021/Day7.kt
@@ -109,7 +109,7 @@ private fun solutionA(input: String): Any {
  * Determine the horizontal position that the crabs can align to using the least fuel possible so they can make
  * you an escape route! How much fuel must they spend to align to that position?
  *
- * Answer: ???
+ * Answer: 92439766
  */
 private fun solutionB(input: String): Any {
     val rows = input.split('\n')
