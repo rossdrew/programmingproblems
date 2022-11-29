99c60373de1fdc209a0ac3dd674df6d3d9163b37
rossdrew <thek1ng13@hotmail.com>
2022-11-29 14:09:54 +0000

    [AoC 2016 Day 1] Part 2 solution
    Just jammed a solution in there. No concern for good design

--- src/main/kotlin/com/rox/adventofcode/y2016/Day1.kt
+++ src/main/kotlin/com/rox/adventofcode/y2016/Day1.kt
@@ -2,6 +2,7 @@ package com.rox.adventofcode.y2016
 
 import com.rox.adventofcode.*
 import com.rox.adventofcode.CardinalDirection.*
+import java.rmi.UnexpectedException
 
 //Answer 5
 private val inputSampleA = """
@@ -18,12 +19,17 @@ private val inputSampleC = """
 R2, R2, R2
 """.trimIndent()
 
+private val inputSampleD = """
+R8, R4, R4, R8
+""".trimIndent()
+
 fun main() {
     //println("Sample Input A: ${solutionA(inputSampleA)}")
     //println("Sample Input B: ${solutionA(inputSampleB)}")
     //println("Sample Input C: ${solutionA(inputSampleC)}")
     println("Part A: ${solutionA(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2016/day1.input"))}")
-    //println("Part B: ${com.rox.adventofcode.y2021.solutionB(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2022/day1.input"))}")
+    //println("Part B, Sample Input C: ${solutionB(inputSampleD)}")
+    println("Part B: ${solutionB(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2016/day1.input"))}")
 }
 
 /**
@@ -54,17 +60,23 @@ fun main() {
  * R5, L5, R5, R3 leaves you 12 blocks away.
  * How many blocks away is Easter Bunny HQ?
  *
- * Answer: ???
- * Incorrect: 73
+ * Answer: 299
  */
 private fun solutionA(input: String): Any {
     val rows = input.split('\n')
 
     val directions = rows[0].split(", ")
     val startState = DirectionalCoord(SimpleCoord(0, 0), NORTH)
+    val finalState = endOfAllSteps(startState, directions)
+
+    //println("\t Ended @ (${finalState.location.x},${finalState.location.y}) facing ${finalState.direction}. Distance = ${manhattanDistance(startState.location, finalState.location)}")
+    return manhattanDistance(startState.location, finalState.location)
+}
+
+private fun endOfAllSteps(startState: DirectionalCoord, directions: List<String>): DirectionalCoord {
     //println("${directions.size} steps...")
 
-    val finalState = directions.fold(startState, { state, nextDirection ->
+    return directions.fold(startState, { state, nextDirection ->
         val steps = Integer.parseInt(nextDirection.substring(1))
         //println("\t @ (${state.location.x},${state.location.y}) facing ${state.direction}. ($nextDirection) Turning ${parseDirection(nextDirection[0], state.direction)} for $steps...")
 
@@ -75,9 +87,6 @@ private fun solutionA(input: String): Any {
             WEST -> DirectionalCoord(SimpleCoord(state.location.x - steps, state.location.y), WEST)
         }
     })
-
-    //println("\t Ended @ (${finalState.location.x},${finalState.location.y}) facing ${finalState.direction}. Distance = ${manhattanDistance(startState.location, finalState.location)}")
-    return manhattanDistance(startState.location, finalState.location)
 }
 
 private fun parseDirection(value: Char, direction: CardinalDirection) : CardinalDirection {
@@ -91,12 +100,70 @@ private fun parseDirection(value: Char, direction: CardinalDirection) : Cardinal
 }
 
 /**
+ * --- Part Two ---
+ * Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually
+ * at the first location you visit twice.
+ *
+ * For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+ *
+ * How many blocks away is the first location you visit twice?
  *
  * Answer: ???
  */
 private fun solutionB(input: String): Any {
     val rows = input.split('\n')
 
-    return input
+    val directions = rows[0].split(", ")
+    val startState = DirectionalCoord(SimpleCoord(0, 0), NORTH)
+    val finalState = firstPathOverlap(startState, directions)
+
+    //println("\t Ended @ (${finalState.location.x},${finalState.location.y}) facing ${finalState.direction}. Distance = ${manhattanDistance(startState.location, finalState.location)}")
+    return manhattanDistance(startState.location, finalState.location)
 }
 
+private fun firstPathOverlap(startState: DirectionalCoord, directions: List<String>): DirectionalCoord {
+    //println("${directions.size} steps...")
+
+    var state = startState
+    val visited = mutableListOf(startState.location)
+    directions.forEach { nextDirection ->
+        val steps = Integer.parseInt(nextDirection.substring(1))
+        //println("\t @ (${state.location.x},${state.location.y}) facing ${state.direction}. ($nextDirection) Turning ${parseDirection(nextDirection[0], state.direction)} for $steps...")
+
+        state = when (parseDirection(nextDirection[0], state.direction)) {
+            NORTH -> {
+                for (y in (state.location.y-steps) until state.location.y){
+                    val newLocation = DirectionalCoord(SimpleCoord(state.location.x, y), NORTH)
+                    if (visited.contains(newLocation.location)) return newLocation
+                    visited.add(SimpleCoord(state.location.x, y))
+                }
+                DirectionalCoord(SimpleCoord(state.location.x, state.location.y - steps), NORTH)
+            }
+            SOUTH -> {
+                for (y in state.location.y + 1 .. (state.location.y + steps)){
+                    val newLocation = DirectionalCoord(SimpleCoord(state.location.x, y), SOUTH)
+                    if (visited.contains(newLocation.location)) return newLocation
+                    visited.add(SimpleCoord(state.location.x, y))
+                }
+                DirectionalCoord(SimpleCoord(state.location.x, state.location.y + steps), SOUTH)
+            }
+            EAST -> {
+                for (x in state.location.x + 1 .. state.location.x+steps){
+                    val newLocation = DirectionalCoord(SimpleCoord(x, state.location.y), EAST)
+                    if (visited.contains(newLocation.location)) return newLocation
+                    visited.add(SimpleCoord(x, state.location.y))
+                }
+                DirectionalCoord(SimpleCoord(state.location.x + steps, state.location.y), EAST)
+            }
+            WEST -> {
+                for (x in state.location.x-steps until state.location.x){
+                    val newLocation = DirectionalCoord(SimpleCoord(x, state.location.y), WEST)
+                    if (visited.contains(newLocation.location)) return newLocation
+                    visited.add(SimpleCoord(x, state.location.y))
+                }
+                DirectionalCoord(SimpleCoord(state.location.x - steps, state.location.y), WEST)
+            }
+        }
+    }
+    throw UnexpectedException("Expected path overlap, none found")
+}
