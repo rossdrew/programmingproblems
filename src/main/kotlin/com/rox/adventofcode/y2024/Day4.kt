d822a4de43a0e1a346ba26f2c07a727a5b7ab938
rossdrew <rossdrew@pingidentity.com>
2024-12-05 20:14:38 +0000

    [AoC 2024 Day 4] Cleanup

--- src/main/kotlin/com/rox/adventofcode/y2024/Day4.kt
+++ src/main/kotlin/com/rox/adventofcode/y2024/Day4.kt
@@ -1,7 +1,6 @@
 package com.rox.adventofcode.y2024
 
 import com.rox.adventofcode.GridCoord
-import com.rox.adventofcode.TwoDGridDirection
 import com.rox.adventofcode.TwoDGridDirection.*
 import com.rox.adventofcode.puzzleInputFromFile
 
@@ -17,20 +16,6 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
 """.trimIndent()
-
-/*
-  0123456789
-0 ....XXMAS. (4,0)->(7,3), (5,0)->(8,0)
-1 .SAMXMS... (4,1)->(1,1)
-2 ...S..A...
-3 ..A.A.MS.X (9,3)->(9,6), (9,3)->(6,6)
-4 XMASAMX.MM (0,4)->(3,4), (6,4)->(6,1), (6,4)->(3,4)
-5 X.....XA.A (0,5)->(3,2), (6,5)->(3,2)
-6 S.S.S.S.SS
-7 .A.A.A.A.A
-8 ..M.M.M.MM
-9 .X.X.XMASX **(1,9)->(4,5)**, (3,9)->(6,6), (3,9)->(0,6), (5,9)->(8,6), (5,9)->(8,9), (5,9)->(2,6), (9,9)->(9,6), (9,9)->(6,6)
- */
 
 fun main() {
     println("Sample Input A: ${solutionA(inputSample)}")
@@ -74,7 +59,7 @@ private fun solutionA(input: String): Any {
 
 /**
  *
- * Answer: ???
+ * Answer: 1948
  */
 private fun solutionB(input: String): Any {
     val rows = input.split('\n')
@@ -88,18 +73,17 @@ private fun solutionB(input: String): Any {
         for (x in 0 until grid[0].size){
             if (grid[y][x] == "A"){
                 val location = GridCoord(grid, x, y)
+                //Horizontals and verticals don't count, filter out any that dont start with 'M' while we're here
                 val relevantNeighbours = location.neighbours().filter { n -> n.cell.value() == "M" && n.direction in setOf(NE, SE, SW, NW) }
 
-                //Horizontals and verticals dont count
                 if (relevantNeighbours.count() > 1){
                     val validIntersections = relevantNeighbours
-                        //List of neighbours (beginning with 'M') intersecting this one which are within the grid
+                        //List of neighbours intersecting this one which are within the grid
                         .map { n -> n.cell.vector(n.direction.inverse(), 2, true).filter { c -> c.withinGrid() } }
                         //Those which spell out MAS
                         .filter { v -> v.map { c -> c.value() }.joinToString ("") == "MAS" }
 
-                    if (validIntersections.count() > 1)
-                        count++
+                    if (validIntersections.count() > 1) count++
                 }
             }
         }
