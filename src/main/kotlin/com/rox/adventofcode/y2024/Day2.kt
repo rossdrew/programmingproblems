aff85e1920856a9b03e82511e2ee8c84eddc2921
rossdrew <rossdrew@pingidentity.com>
2024-12-17 10:25:12 +0000

    [AoC 2024 Day 2] An attempt at part B
    Still not working, can't figure out why

--- src/main/kotlin/com/rox/adventofcode/y2024/Day2.kt
+++ src/main/kotlin/com/rox/adventofcode/y2024/Day2.kt
@@ -15,8 +15,8 @@ private val inputSample = """
 fun main() {
     println("Sample Input A: ${solutionA(inputSample)}")
     println("Sample Input B: ${solutionB(inputSample)}")
-    //println("Part A: ${solutionA(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2024/Day2.input"))}")
-    //println("Part B: ${solutionB(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y202X/DayX.input"))}")
+    println("Part A: ${solutionA(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2024/Day2.input"))}")
+    println("Part B: ${solutionB(puzzleInputFromFile("src/main/kotlin/com/rox/adventofcode/y2024/Day2.input"))}")
 }
 
 enum class State {
@@ -24,56 +24,59 @@ enum class State {
 }
 
 /**
- *
- *
  * Answer: 202
  */
 private fun solutionA(input: String): Any {
     val rows = input.split('\n')
     val columnSeperatedRows = rows.map { row -> row.split(' ') }
 
-    return safeRows(columnSeperatedRows)
+    return columnSeperatedRows.map { row -> rowSafe(row) }.count { it }
 }
 
 /**
- *
- * Answer: ???
+ * Answer: 268 (TOO LOW)
  */
 private fun solutionB(input: String): Any {
     val rows = input.split('\n')
     val columnSeperatedRows = rows.map { row -> row.split(' ') }
 
-    return "TODO"
+    return columnSeperatedRows.map { row -> rowSafe(row, 1) }.count { it }
 }
 
-private fun safeRows(columnSeperatedRows: List<List<String>>): Int {
-    var safeRows = 0
+private fun rowSafe(row: List<String>, tolerance: Int = 0, indent: String = ""): Boolean {
+    var expectedState = State.UNKNOWN
+    for (column in 1 until row.size){
+        val current = row[column].toInt()
+        val previous = row[column - 1].toInt()
 
-    row@ for (row in columnSeperatedRows.indices) {
-        var expectedState = State.UNKNOWN
-        for (column in 1 until columnSeperatedRows[row].size) {
-            val current = columnSeperatedRows[row][column].toInt()
-            val previous = columnSeperatedRows[row][column - 1].toInt()
-
-            //UNSAFE: Difference is not 1-3
-            if (previous == current || abs(current - previous) > 3) {
-                //println("UNSAFE (1): ${columnSeperatedRows[row]}")
-                continue@row
-            }
-
-            val currentState = if (current > previous) State.INC else State.DEC
-
-            if (expectedState == State.UNKNOWN) {
-                expectedState = currentState
-            } else if (expectedState != currentState) {
-                //UNSAFE: Switched between increasing and decreasing
-                //println("UNSAFE (2): ${columnSeperatedRows[row]}. ($current -> $previous), ${currentState} when expecting ${expectedState}")
-                continue@row
-            }
+        //UNSAFE: Difference is not 1-3
+        if (previous == current || abs(current - previous) > 3) {
+            println("${indent}UNSAFE (high step @ $column): $row, difference was ${current-previous} between $current & $previous")
+            return toleranceCheck(tolerance, row, column)
         }
-        //println("SAFE: ${columnSeperatedRows[row]}")
-        safeRows++
-    }
 
-    return safeRows
+        val currentState = if (current > previous) State.INC else State.DEC
+
+        if (expectedState == State.UNKNOWN) {
+            expectedState = currentState
+        } else if (expectedState != currentState) {
+            //UNSAFE: Switched between increasing and decreasing
+            println("${indent}UNSAFE (switched direction @ $column): ${row}. ($previous -> $current), $currentState when expecting $expectedState")
+            return toleranceCheck(tolerance, row, column)
+        }
+    }
+    println("${indent}SAFE: $row")
+    return true
+}
+
+private fun toleranceCheck(tolerance: Int, row: List<String>, column: Int): Boolean {
+    return if (tolerance == 1) {
+        val a = row.filterIndexed { i, _ -> i != column }
+        val b = row.filterIndexed { i, _ -> i != column - 1 }
+        println("\tTrying with $a & $b")
+
+        rowSafe(b,0, "\t\t") || rowSafe(a,0, "\t\t")
+    } else {
+        false
+    }
 }
